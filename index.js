2d879465d42504e52b4b0fe9afb14c8ef3886ead
UbitaLaUva <ubitaritar@gmail.com>
2025-03-01 11:19:20 -0500

    3 commit

--- index.js
+++ index.js
@@ -192,10 +192,8 @@ app.get('/', (req, res) => {
 </div>
 
 <div class="social-buttons">
-    <button onclick="window.open('https://discord.com', '_blank')">
-    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/discord-logo.png" alt="discord-logo"/>
-    Discord
-</button>
+    <button onclick="window.open('https://discord.gg/TXjtDxSnf2', '_blank')">
+    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/discord-logo.png" alt="discord-logo"/></button>
     <button onclick="window.open('https://www.youtube.com', '_blank')">YouTube</button>
     <button onclick="window.open('https://www.paypal.com', '_blank')">PayPal</button>
 </div>
