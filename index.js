559147cbdaabe4cacc4f11260207dd5fcbeeed5d
UbitaLaUva <ubitaritar@gmail.com>
2025-03-01 14:01:31 -0500

    addVerificatorLink commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://lootdest.org',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
