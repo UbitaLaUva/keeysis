fcfedc208f4035dc18c0c6031284adba62282c82
UbitaLaUva <ubitaritar@gmail.com>
2025-03-01 19:27:18 -0500

    addVerificatorLink commit

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ app.get('/', (req, res) => {
     <input type="text" id="key" readonly>
     <button onclick="copyKey()">Copiar Key</button>
 </div>
+<!-- Publicidad Izquierda -->
 <div class="ad-container" style="position: absolute; left: 10%; top: 50%; transform: translateY(-50%); width: 300px; height: 250px; box-shadow: 0 0 20px #09f; border: 2px solid #09f; padding: 10px; border-radius: 10px; background: rgba(0, 0, 0, 0.7);">
     <script type="text/javascript">
         atOptions = {
@@ -171,6 +172,23 @@ app.get('/', (req, res) => {
     </script>
     <script type="text/javascript" src="//www.highperformanceformat.com/d3566c394d8e84b8adf54366869f2054/invoke.js"></script>
 </div>
+
+<!-- Publicidad Derecha -->
+<div class="ad-container" style="position: absolute; right: 10%; top: 50%; transform: translateY(-50%); width: 300px; height: 250px; box-shadow: 0 0 20px #09f; border: 2px solid #09f; padding: 10px; border-radius: 10px; background: rgba(0, 0, 0, 0.7);">
+    <script type="text/javascript">
+        atOptions = {
+            'key': 'd3566c394d8e84b8adf54366869f2054',
+            'format': 'iframe',
+            'height': 250,
+            'width': 300,
+            'params': {}
+        };
+    </script>
+    <script type="text/javascript" src="//www.highperformanceformat.com/d3566c394d8e84b8adf54366869f2054/invoke.js"></script>
+</div>
+
+
+
 
 <script>
     async function generateKey() {
